99bb406c030450df48a112dd40a7db8ef69f3f02
Jason <jasonbohio@gmail.com>
2024-05-02 15:00:28 -0400

    2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -115,10 +115,10 @@
     });
 
     const now = Math.floor(Date.now() / 1000);
-    const lastWeek = now - (7 * 24 * 60 * 60);
+    const lastTwoWeek = now - (14 * 24 * 60 * 60);
 
     function fetchEventFromSub() {
-        const sub = ndk.subscribe({ kinds: [1], created_at: { $gte: lastWeek } }, { closeOnEose: false });
+        const sub = ndk.subscribe({ kinds: [1], created_at: { $gte: lastTwoWeek } }, { closeOnEose: false });
         let matchedEvents = [];
         let combinedEvents = {};
 
