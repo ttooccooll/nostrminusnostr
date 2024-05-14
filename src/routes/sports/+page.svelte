fb299a093aa19a2aee2bb86cf4b4bdf899777f90
Jason <jasonbohio@gmail.com>
2024-05-14 12:57:59 -0400

    text

--- src/routes/sports/+page.svelte
+++ src/routes/sports/+page.svelte
@@ -440,16 +440,16 @@
 </div>
 
 <footer>
-    <p class="text" style="font-size: 20px">
+    <p class="text" style="font-size: 20px, font-family: Droid Serif">
         NOSTRMINUSNOSTR presents INTERNATIONAL SPORTS UNCENSORED
     </p>
-    <p class="date">
+    <p class="date, font-family: Droid Serif">
         {formattedDate}
     </p>
-    <p class="text">
+    <p class="text, font-family: Droid Serif">
         Welcome to nostrminusnostr, sports edition. Here you will find an international feed of sports related content, powered by the internet's most censorship-resistant protocol. There is no need to login if you just hope to catch up on the news. If you are familiar with the practice of zapping, and would like to login, you can use any web-extention signer.
     </p>
-    <p class="date" style="margin-bottom: 10px">
+    <p class="date" style="margin-bottom: 10px, font-family: Droid Serif">
         It takes a moment to load up the notes. Please be patient and know that if you can still read the "hold your horses" statements above, the program is still loading.
     </p>
 </footer>
