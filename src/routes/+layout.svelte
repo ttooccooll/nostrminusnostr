19721ee0d2439bbbf6f39313e66a468dd7301dbe
Jason <jasonbohio@gmail.com>
2024-05-15 17:09:33 -0400

    navhover

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,6 @@
 <script>
     import '../app.css';
-
-    function navigateWithDelay(url) {
-        setTimeout(() => {
-            goto(url);
-        }, 500);
-    }
-
-    document.querySelectorAll('nav a').forEach(function(link) {
-        link.removeEventListener('mouseover', hoverEventHandler);
-        link.removeEventListener('mouseout', hoverEventHandler);
-    });
 </script>
-
-<style>
-  nav a:hover {
-    color: inherit !important;
-    background-color: transparent !important;
-  }
-</style>
 
 <div class="everybody">
     <h1>nostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminusnostrminus</h1>
@@ -38,10 +20,3 @@
     It's like <a href="https://garfieldminusgarfield.net" target="_blank">garfieldminusgarfield</a>, but not really.
   </p>
 </footer>
-
-<nav>
-  <a on:click={() => navigateWithDelay('nostrminusnostr.vercel.app')} href="/">Home</a>
-  <a on:click={() => navigateWithDelay('nostrminusnostr.vercel.app/pizza')} href="/pizza">Pizza Party</a>
-  <a on:click={() => navigateWithDelay('nostrminusnostr.vercel.app/sports')} href="/sports">Sports</a>
-  <a href="https://www.youtube.com/watch?v=sQnoZUR6fvY" target="_blank">Alt Coins</a>
-</nav>
