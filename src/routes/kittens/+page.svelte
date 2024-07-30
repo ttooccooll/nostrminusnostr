42da932931aadb06bf4903bdaab3f997faa8236a
Jason <jasonbohio@gmail.com>
2024-07-30 14:35:44 -0400

    kittens

--- src/routes/kittens/+page.svelte
+++ src/routes/kittens/+page.svelte
@@ -8,7 +8,7 @@
     import './kittens.css';
 
     const ndk = new NDK({
-        explicitRelayUrls: [ "wss://nos.lol", "wss://relay.snort.social", "wss://relay.nostr.band" ],
+        explicitRelayUrls: [ "wss://relay.bitcoinpark.com", "wss://nos.lol", "wss://relay.snort.social", "wss://relay.nostr.band" ],
     });
 
     let isLoading = true;
