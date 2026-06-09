d18a548b1c3495414f6eaf0161acad833549afde
ttooccooll <jasonbohio@gmail.com>
2026-06-08 22:16:25 -0400

    Add missing 7 relays to kittens page for consistent 25-relay config

--- src/routes/kittens/+page.svelte
+++ src/routes/kittens/+page.svelte
@@ -8,7 +8,7 @@
     import './kittens.css';
 
     const ndk = new NDK({
-        explicitRelayUrls: [ "wss://nostr.mom", "wss://relay.primal.net", "wss://nos.lol", "wss://nostr.thank.eu", "wss://nostr.wine", "wss://relay.damus.io", "wss://purplepag.es", "wss://lunchbox.sandwich.farm", "wss://fiatjaf.com", "wss://relay.snort.social", "wss://eden.nostr.land", "wss://offchain.pub", "wss://nostr.bitcoiner.social", "wss://yabu.me", "wss://relay.nostr.sc", "wss://relay.nostr.info", "wss://nostr.oxtr.dev", "wss://relay.0xchat.com" ],
+        explicitRelayUrls: [ "wss://nostr.mom", "wss://relay.primal.net", "wss://nos.lol", "wss://nostr.thank.eu", "wss://nostr.wine", "wss://relay.damus.io", "wss://purplepag.es", "wss://lunchbox.sandwich.farm", "wss://fiatjaf.com", "wss://relay.snort.social", "wss://eden.nostr.land", "wss://offchain.pub", "wss://nostr.bitcoiner.social", "wss://yabu.me", "wss://relay.nostr.sc", "wss://relay.nostr.info", "wss://nostr.oxtr.dev", "wss://relay.0xchat.com", "wss://nostr.8777.ch", "wss://nostr.land", "wss://nostr-01.yakihonne.com", "wss://nostr.superfriends.online", "wss://relay.nos.social", "wss://relay.getalby.com", "wss://relay.nostr.net" ],
     });
 
     let sub = null;
